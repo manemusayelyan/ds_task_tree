1fb2166511f851abf0a556929e625b07d7fe352b
Mane Musayelyan <81154606+manemusayelyan@users.noreply.github.com>
2023-12-24 16:37:50 +0400

    Update ds_tree_23.cpp

--- ds_tree_23.cpp
+++ ds_tree_23.cpp
@@ -23,6 +23,16 @@ t_node* create_new_node(int data)
 	return (tmp);
 }
 
+void delete_tree(t_node* root)
+{
+    if (root == NULL)
+        return;
+
+    delete_tree(root->left);
+    delete_tree(root->right);
+    delete root;
+}
+
 void preorder_iterative(t_node* root)
 {
 	if (root == NULL)
