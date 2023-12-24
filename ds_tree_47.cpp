9e74e3df2a97299297318c4ef7779d379bb37940
Mane Musayelyan <81154606+manemusayelyan@users.noreply.github.com>
2023-12-24 16:24:04 +0400

    Add files via upload

---
+++ ds_tree_47.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <queue>
+#include <climits>
+using namespace std;
+
+typedef struct s_node
+{
+    int val;
+    struct s_node* left;
+    struct s_node* right;
+    s_node(int x) : val(x), left(nullptr), right(nullptr) {}
+} t_node;
+
+int find_level_of_max(t_node* root) {
+    if (root == nullptr)
+        return 0;
+
+    int max_val = INT_MIN;
+    int level = 0;
+
+    queue<t_node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int size = q.size();
+
+        for (int i = 0; i < size; ++i)
+        {
+            t_node* current = q.front();
+            q.pop();
+
+            if (current->val > max_val)
+            {
+                max_val = current->val;
+                level = size;
+            }
+
+            if (current->left)
+                q.push(current->left);
+
+            if (current->right)
+                q.push(current->right);
+        }
+    }
+
+    return level;
+}
+
+int count_nodes_at_level(t_node* root, int target_level) {
+    if (root == nullptr)
+        return 0;
+
+    int current_level = 1;
+    queue<t_node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int size = q.size();
+
+        for (int i = 0; i < size; ++i) {
+            t_node* current = q.front();
+            q.pop();
+
+            if (current_level == target_level) {
+                return size;
+            }
+
+            if (current->left)
+                q.push(current->left);
+
+            if (current->right)
+                q.push(current->right);
+        }
+
+        current_level++;
+    }
+
+    return 0;
+}
+
+int main() {
+    t_node* root = new t_node(3);
+    root->left = new t_node(5);
+    root->right = new t_node(8);
+    root->left->left = new t_node(1);
+    root->left->right = new t_node(4);
+    root->right->left = new t_node(6);
+
+    int target_level = find_level_of_max(root);
+
+    if (target_level > 0) {
+        int node_count = count_nodes_at_level(root, target_level);
+        cout << target_level << " " << node_count << endl;
+    }
+    else
+        cout << "0\n" << endl;
+
+    return 0;
+}
