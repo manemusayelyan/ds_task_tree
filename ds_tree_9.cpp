eb22ec9bc62f7b09ef42a9e698b6e984ccc253c5
Mane Musayelyan <81154606+manemusayelyan@users.noreply.github.com>
2023-12-24 15:20:27 +0400

    Add files via upload

---
+++ ds_tree_9.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+typedef struct s_node
+{
+    int data;
+    s_node* left;
+    s_node* right;
+
+    s_node(int data) : data(data), left(nullptr), right(nullptr) {}
+} t_node;
+
+int count_leaves_recursive(t_node* root)
+{
+    if (root == nullptr)
+        return 0;
+
+    if (root->left == nullptr && root->right == nullptr)
+        return 1;
+    int left_leaves = count_leaves_recursive(root->left);
+    int right_leaves = count_leaves_recursive(root->right);
+
+    return left_leaves + right_leaves;
+}
+
+int count_leaves_iterative(t_node* root)
+{
+    if (root == nullptr)
+        return 0;
+
+    int leaves = 0;
+    stack<t_node*> st;
+    st.push(root);
+
+    while (!st.empty())
+    {
+        t_node* cur = st.top();
+        st.pop();
+
+        if (!cur->left && !cur->right)
+            leaves++;
+
+        if (cur->right)
+            st.push(cur->right);
+
+        if (cur->left)
+            st.push(cur->left);
+    }
+
+    return leaves;
+}
+
+int main()
+{
+    t_node* root = new s_node(1);
+    root->left = new s_node(2);
+    root->right = new s_node(3);
+    root->left->left = new s_node(4);
+    root->left->right = new s_node(5);
+
+    cout << "Number of leaves in the binary tree: (Recursive) " << count_leaves_recursive(root) << endl;
+    cout << "Number of leaves in the binary tree: (Iterative) " << count_leaves_iterative(root) << endl;
+    return 0;
+}
